06118ba13fca8cb69bf39a47188880f732cdb1ed
Brajendra Pratap singh <bp040645@gmail.com>
2026-09-21 23:59:42 +0530

    numbring

---
+++ numberprinting.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i ,n;
+    cout<<"ENTER NUMBER FOR COUNTING";
+    cin>>n;
+    for(i=1;i<=n;i++){
+        cout<<"the number is : "<<i<<endl;
+    }
+}
